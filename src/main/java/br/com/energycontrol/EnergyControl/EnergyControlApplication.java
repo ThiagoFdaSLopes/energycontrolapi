3c8a4762542becbf39e6d7677a54946fd6752986
Thiago Lopes <programadorthiagolopes@gmail.com>
2025-04-30 19:10:19 -0300

    feat: start project

---
+++ src/main/java/br/com/energycontrol/EnergyControl/EnergyControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.energycontrol.EnergyControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/energycontrol/EnergyControl/EnergyControlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.energycontrol.EnergyControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

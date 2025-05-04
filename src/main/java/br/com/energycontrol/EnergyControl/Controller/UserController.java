c70f9e78fce241bd660011223556447397476698
Thiago Lopes <programadorthiagolopes@gmail.com>
2025-05-04 17:41:22 -0300

    feat: controller user

---
+++ src/main/java/br/com/energycontrol/EnergyControl/Controller/UserController.java
@@ -0,0 +1,49 @@
+package br.com.energycontrol.EnergyControl.Controller;
+
+import br.com.energycontrol.EnergyControl.Dto.UserDTO;
+import br.com.energycontrol.EnergyControl.Dto.UserRegisterDTO;
+import br.com.energycontrol.EnergyControl.Model.User;
+import br.com.energycontrol.EnergyControl.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/users")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDTO save(@RequestBody UserRegisterDTO user){
+        return userService.saveUser(user);
+    }
+
+    @GetMapping("/users")
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserDTO> listAll(){
+        return userService.listAllUsers();
+    }
+
+    @GetMapping("/users/{userId}")
+    public UserDTO searchById(@PathVariable Long userId){
+        return userService.searchById(userId);
+    }
+
+    @DeleteMapping("/users/{userId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long userId){
+        userService.delete(userId);
+    }
+
+    @PutMapping("/users")
+    @ResponseStatus(HttpStatus.OK)
+    public User update(@RequestBody User user){
+        return userService.update(user);
+    }
+
+}
